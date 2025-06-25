54393138b4d8f3f1e6802a0aa425723c922fa3fd
sagar rajak <sagarrajak245@gmail.com>
2025-06-26 01:51:53 +0530

    26 jul -2

--- cycle_in_graph_bfs.cpp
+++ cycle_in_graph_bfs.cpp
@@ -31,38 +31,40 @@ public:
         }
         return false; // no cycle detected
     }
+}
 
-    int main()
+int
+main()
+{
+    int n, m;
+    cout << "Enter number of nodes and edges: ";
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n);
+    cout << "Enter edges (u v):" << endl;
+    for (int i = 0; i < m; i++)
     {
-        int n, m;
-        cout << "Enter number of nodes and edges: ";
-        cin >> n >> m;
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // undirected graph
+    }
 
-        vector<vector<int>> adj(n);
-        cout << "Enter edges (u v):" << endl;
-        for (int i = 0; i < m; i++)
+    vector<int> visited(n, 0);
+    vector<int> parent(n, -1);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
         {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u); // undirected graph
-        }
-
-        vector<int> visited(n, 0);
-        vector<int> parent(n, -1);
-
-        for (int i = 0; i < n; i++)
-        {
-            if (!visited[i])
+            if (bfs_cycle(i, visited, parent, adj))
             {
-                if (bfs_cycle(i, visited, parent, adj))
-                {
-                    cout << "Cycle detected in the graph." << endl;
-                    return 0;
-                }
+                cout << "Cycle detected in the graph." << endl;
+                return 0;
             }
         }
-
-        cout << "No cycle detected in the graph." << endl;
-        return 0;
     }
+
+    cout << "No cycle detected in the graph." << endl;
+    return 0;
+}
